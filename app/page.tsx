da2141eff0da9ccf2071fa430fc5bafa02007c3e
Roshan Bansie <roshan_b@outlook.com>
2026-01-18 19:53:40 +0100

    Hero sectie aangepast.

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
 import "./page.css";
+import Hero from "@/components/home/hero/Hero";
 import Projects from "@/components/home/Projects";
 
 export default function Home() {
   return (
       <main>
-        <div className="hero">
-          <h1>Turning design into reality</h1>
-          <p>
-            Lorem ipsum dolor sit amet. 
-            Ab quam laborum est nulla dolores in delectus consequatur et 
-            quam porro ut ducimus mollitia et quia eligendi. 
-            Eos eaque nostrum ut accusamus odit sed omnis cumque est autem corrupti 
-            sed tempora sequi sed libero officia. 
-            33 deleniti accusamus ut asperiores excepturi 
-            ad accusamus mollitia vel velit sint ut molestiae odio
-            sit possimus veniam ut temporibus rerum.
-          </p>
-        </div>
+        <Hero />
         <Projects />
       </main>
   );
---
+++ components/home/hero/Hero.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import React from "react"
+import { HiDownload } from "react-icons/hi"
+import { MdFileDownloadDone } from "react-icons/md"
+import { FiGithub } from "react-icons/fi"
+import Link from "next/link"
+import "./hero.css"
+
+export default function Hero() {
+
+    const [hasDownloaded, setHasDownloaded] = React.useState<boolean>(false)
+
+    return (
+        <section className="hero">
+          <h1>Hey, my name is Roshan</h1>
+          <p>
+            Computer Science student developing full-stack apps that solve everyday problems
+            using React.js, Typescript, Next.js, Express.js, Node.js and SQL. 
+          </p>
+          <div className="hero-buttons">
+            <button className="primary-button" onClick={() => setHasDownloaded(true)}>
+                {
+                    hasDownloaded ? <MdFileDownloadDone className="hero-btn-icon" /> 
+                    : <HiDownload className="hero-btn-icon" />
+                }
+                Resume
+            </button>
+            <button className="secondary-button">
+                <Link href="https://github.com/RoshanBansie" target="_blank" className="github-link">
+                    <FiGithub className="hero-btn-icon" />
+                    Github
+                </Link>
+            </button>
+          </div>
+        </section>
+    )
+}
