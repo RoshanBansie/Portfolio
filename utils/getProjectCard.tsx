133fea11e6af723a97ac6a97d5637e04cca78d42
Roshan Bansie <roshan_b@outlook.com>
2026-03-03 11:46:50 +0100

    Project card style aangepast en skills sectie style aangepast.

--- utils/getProjectCard.tsx
+++ utils/getProjectCard.tsx
@@ -10,25 +10,31 @@
         const {src, width, height, alt} = image
         return (
             <div className="project-card" key={"project-"+title}>
-                <ImageUI
-                    src={src}
-                    originalWidth={width}
-                    originalHeight={height}
-                    height="25rem"
-                    width="100%"
-                    alt={alt}
-                    className="project-card-image-desktop"
-                />
-                <ImageUI
-                    src={src}
-                    originalWidth={width}
-                    originalHeight={height}
-                    height="15rem"
-                    width="100%"
-                    alt={alt}
-                    className="project-card-image-mobile"
-                />
+                <div className="project-card-img-container">
+                    <Link href={link} >
+                        <ImageUI
+                            src={src}
+                            originalWidth={width}
+                            originalHeight={height}
+                            height="25rem"
+                            width="100%"
+                            alt={alt}
+                            className="project-card-image-desktop"
+                        />
+                        <ImageUI
+                            src={src}
+                            originalWidth={width}
+                            originalHeight={height}
+                            height="15rem"
+                            width="100%"
+                            alt={alt}
+                            className="project-card-image-mobile"
+                        />
+                    </Link>
+                </div>
                 <div className="project-description">
+                    <h3>{title}</h3>
+                    <p>{description}</p>
                     <div className="project-tags-container">
                         {tags.map(tag => (
                             <div className="project-tag" key={"tag-"+tag}>
@@ -36,11 +42,6 @@
                             </div>
                         ))}
                     </div>
-                    <h3>{title}</h3>
-                    <p>{description}</p>
-                    <button className="primary-button">
-                        <Link href={link}>Read more</Link>
-                    </button>
                 </div>
             </div>
         )
