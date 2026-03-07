f01a0b93cb3809d49b85a6bebc6f7090832f3202
Roshan Bansie <roshan_b@outlook.com>
2026-03-07 01:13:39 +0100

    Email rendering gefixt en juiste format voor sender.

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,39 +1,46 @@
+import React from 'react';
 import { Resend } from 'resend';
 import ContactMail from '@/templates/ContactMail';
+import { render } from '@react-email/components';
 import type { ContactFormData } from '@/app/types';
 
-const resend = new Resend(process.env.RESEND_API_KEY)
-const EMAIL = process.env.EMAIL
-
 export async function POST(req: Request) {
-  try {
-    // Receive formData.
-    const { fullName, email, subject, message }: ContactFormData = await req.json()
-    if (!fullName || !email || !subject || !message) {
-      throw new Error("One of the input field data is undefined")
-    }
-
-    // Send email
-    const { data, error } = await resend.emails.send({
-      from: `Contact <${EMAIL}>`,
-      to: [EMAIL!],
-      subject: subject,
-      replyTo: email,
-      react: ContactMail({
-        fullName: fullName,
-        email: email,
-        subject: subject,
-        message: message
-      }),
-    })
-
-    if (error) {
-      return Response.json({ error }, { status: 500 })
-    }
-    return Response.json(data)
-  
-  } catch (error) {
-    console.log(error)
-    return Response.json({ error }, { status: 500 })
+  const { fullName, email, subject, message }: ContactFormData = await req.json()
+  if (!fullName || !email || !subject || !message) {
+    throw new Error("One of the input field data is undefined")
   }
+
+  const RESEND_API_KEY = process.env.RESEND_API_KEY
+  const DOMAIN = process.env.DOMAIN
+  const EMAIL = process.env.EMAIL
+  
+  const resend = new Resend(RESEND_API_KEY)
+    async function send(sender, receiver, replyTo, subject, content) {
+        const {data, error} = await resend.emails.send({
+            from: sender,
+            to: receiver,
+            replyTo: replyTo,
+            subject: subject,
+            html: content
+        })
+        
+        if (error) {
+            console.error(error)
+            return Response.json({ error }, { status: 500 })
+        }
+    }
+
+    try {
+        const sender = `Contact <info@${DOMAIN}>`
+        const emailHtml = await render(React.createElement(ContactMail, {
+            fullName, email, subject, message
+        }))
+        await send(sender, [EMAIL], email, subject, emailHtml)
+  
+    } catch(error) {
+      if (error instanceof Error) {
+        console.error(`Contact mailer: ${error}`)
+        return Response.json({ error }, { status: 500 })
+      }
+    }
 }
